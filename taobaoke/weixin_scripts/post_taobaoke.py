de72fd0460922b4b5824faf7678fcd5cb329fd00
Adam <934399003@qq.com>
2017-10-20 15:21:22 +0800

    1.新增接口，从酷Q插件收到POST请求，解析商品优惠信息，存库并推送至微信群， 复用post_taobaoke.select()方法。 \n2.修改post_taobaoke，select()和post_taobaoke_url()新增参数p,应为Product实例，默认为None。post_taobaoke_url()中加入对p的判断，p为None时为原逻辑（从库中选取商品），p不为Noone直接发送实例p对应商品。  \n3.新增依赖selenium，requirements.txt已一并更新，另须于服务器下载Phantomjs，且entry_views中变量executable_path应为服务器上phantomjss的执行路径。

--- taobaoke/weixin_scripts/post_taobaoke.py
+++ taobaoke/weixin_scripts/post_taobaoke.py
@@ -36,7 +36,7 @@ import logging
 logger = logging.getLogger('post_taobaoke')
 
 
-def post_taobaoke_url(wx_id, group_id, md_username):
+def post_taobaoke_url(wx_id, group_id, md_username, p=None):
     # 发单人的wx_id, 群的id, 手机号
     try:
         tk_user = TkUser.get_user(md_username)
@@ -47,27 +47,28 @@ def post_taobaoke_url(wx_id, group_id, md_username):
     except Exception as e:
         logger.error('{0} 获取Adzone.pid失败, reason: {1}'.format(wx_id, e))
 
-    qs = Product.objects.filter(
-        ~Q(pushrecord__group__contains=group_id,
-           pushrecord__create_time__gt=timezone.now() - datetime.timedelta(days=3)),
-        available=True, last_update__gt=timezone.now() - datetime.timedelta(hours=4),
-    )
-
-    # 用发送过的随机商品替代
-    if qs.count() == 0:
+    if not p:
         qs = Product.objects.filter(
+            ~Q(pushrecord__group__contains=group_id,
+               pushrecord__create_time__gt=timezone.now() - datetime.timedelta(days=3)),
             available=True, last_update__gt=timezone.now() - datetime.timedelta(hours=4),
         )
-        beary_chat('点金推送商品失败：无可用商品')
 
-    for _ in range(50):
-        try:
-            r = random.randint(0, qs.count() - 1)
-            p = qs.all()[r]
-            break
-        except Exception as exc:
-            print "Get entry exception. Count=%d." % qs.count()
-            logger.error(exc)
+        # 用发送过的随机商品替代
+        if qs.count() == 0:
+            qs = Product.objects.filter(
+                available=True, last_update__gt=timezone.now() - datetime.timedelta(hours=4),
+            )
+            beary_chat('点金推送商品失败：无可用商品')
+
+        for _ in range(50):
+            try:
+                r = random.randint(0, qs.count() - 1)
+                p = qs.all()[r]
+                break
+            except Exception as exc:
+                print "Get entry exception. Count=%d." % qs.count()
+                logger.error(exc)
 
     # img or text
     text_msg_dict = {
@@ -97,7 +98,7 @@ def post_taobaoke_url(wx_id, group_id, md_username):
 
 
 
-def select():
+def select(p=None):
     # 筛选出已经登录的User
     user_list = WxUser.objects.filter(login__gt = 0).all()
     logger.info([user.username for user in user_list])
@@ -129,10 +130,11 @@ def select():
                     logger.info(u'%s 向 %s 推送商品' % (wxuser.nickname, chatroom.nickname))
 
                     import thread
-                    thread.start_new_thread(post_taobaoke_url, (wx_id, group_id, md_username))
+                    thread.start_new_thread(post_taobaoke_url, (wx_id, group_id, md_username,p))
                 except Exception as e:
                     logging.error(e)
                     print(e)
+
 
 if __name__ == "__main__":
     while True:
