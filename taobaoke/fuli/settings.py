dd3a5dbdb56603926b28240ac13d440bcd51f6e1
752546103 <752546103@qq.com>
2017-10-28 10:31:50 +0800

    [添加测试服务器]

--- taobaoke/fuli/settings.py
+++ taobaoke/fuli/settings.py
@@ -101,29 +101,29 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {'charset': 'utf8mb4'},
+#         'NAME': 'ipad_weixin',
+#         'USER': 'root',
+#         'PASSWORD': 'Xiaozuanfeng',
+#         'HOST': 's-prod-02.qunzhu666.com',
+#         'PORT': '50001',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {'charset': 'utf8mb4'},
         'NAME': 'ipad_weixin',
         'USER': 'root',
-        'PASSWORD': 'Xiaozuanfeng',
-        'HOST': 's-prod-02.qunzhu666.com',
-        'PORT': '50001',
+        'PASSWORD': 'keyerror',
     }
 }
-
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {'charset': 'utf8mb4'},
-#         'NAME': 'ipad_weixin_02',
-#         'USER': 'root',
-#         'PASSWORD': 'keyerror',
-#     }
-# }
 
 
 
