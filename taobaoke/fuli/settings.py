cf0b24fa815201c5815443e6cebcc4ac7ff3cc60
752546103 <752546103@qq.com>
2017-10-30 17:32:55 +0800

    [merge]修改数据库配置

--- taobaoke/fuli/settings.py
+++ taobaoke/fuli/settings.py
@@ -102,29 +102,18 @@ CSRF_COOKIE_SECURE = True
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {'charset': 'utf8mb4'},
-#         'NAME': 'guofenjie',
-#         'USER': 'root',
-#         'PASSWORD': 'Xiaozuanfeng',
-#         'HOST': 's-prod-02.qunzhu666.com',
-#         'PORT': '50001',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {'charset': 'utf8mb4'},
-        'NAME': 'adam_test',
+        'NAME': 'guofenjie',
         'USER': 'root',
-        'PASSWORD': 'qazwsx102938',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PASSWORD': 'Xiaozuanfeng',
+        'HOST': 's-prod-02.qunzhu666.com',
+        'PORT': '50001',
     }
 }
+
 
 # DATABASES = {
 #     'default': {
