f83deac6840002e3517cf1e512888caa2afceb2e
752546103 <752546103@qq.com>
2017-11-09 11:19:11 +0800

    [repair]修正MergeRequest带来的本地配置文件

--- taobaoke/apps/broadcast/views/server_settings.py
+++ taobaoke/apps/broadcast/views/server_settings.py
@@ -1,7 +1,5 @@
 # _*_ coding:utf-8 _*_
 S_PROD_04_INT = '10.29.253.179'
 S_PROD_07_INT = '10.30.2.54'
-# S_POC_01_INT = 's-poc-01.qunzhu666.com'
-S_POC_01_INT = 'localhost'
-# REDIS_PORT = 50002
-REDIS_PORT = 6379
+S_POC_01_INT = 's-poc-01.qunzhu666.com'
+REDIS_PORT = 50002
