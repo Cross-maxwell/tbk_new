43805355e89b06572b5a05089e4c0806b02502b1
752546103 <752546103@qq.com>
2017-11-20 17:57:46 +0800

    [update]添加线上DB

--- taobaoke/fuli/settings.py
+++ taobaoke/fuli/settings.py
@@ -102,29 +102,29 @@ CSRF_COOKIE_SECURE = True
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {'charset': 'utf8mb4'},
-#         'NAME': 'mmt',
-#         'USER': 'root',
-#         'PASSWORD': 'Xiaozuanfeng',
-#         'HOST': 's-prod-02.qunzhu666.com',
-#         'PORT': '50001',
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {'charset': 'utf8mb4'},
-        'NAME': 'new_taobaoke_sys',
+        'NAME': 'mmt',
         'USER': 'root',
-        'PASSWORD': 'keyerror',
+        'PASSWORD': 'Xiaozuanfeng',
+        'HOST': 's-prod-02.qunzhu666.com',
+        'PORT': '50001',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {'charset': 'utf8mb4'},
+#         'NAME': 'new_taobaoke_sys',
+#         'USER': 'root',
+#         'PASSWORD': 'keyerror',
+#     }
+# }
 
 
 
