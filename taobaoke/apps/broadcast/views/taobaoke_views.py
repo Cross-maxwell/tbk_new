e15056a384b9f697de2a4a51ba7f52e99988b7ba
752546103 <752546103@qq.com>
2017-12-05 09:50:28 +0800

    [update]修改SendArtificalMsg每个用户的延时为3.5秒

--- taobaoke/apps/broadcast/views/taobaoke_views.py
+++ taobaoke/apps/broadcast/views/taobaoke_views.py
@@ -564,7 +564,7 @@ class SendArtificialMsg(View):
                 logger.info("SendArtificialMsg response: {}".format(send_msg_response.content))
 
                 artifical_data = req_dict["data"]
-                time.sleep(1.5)
+                time.sleep(3.5)
             except Exception as e:
                 logger.error(e)
 
