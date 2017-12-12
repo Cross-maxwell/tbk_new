cd6d0f6664a059df69ea6e713ab2a6780685a8c2
renyangfar <renyangfar@gmail.com>
2017-12-12 16:43:19 +0800

    增加刷新群二维码功能，需要运行脚本qrcode_manage.py,同时在broadcast中增加了一个表，需要迁移。。。

--- taobaoke/scripts/wx_chatroom/qrcode_manage.py
+++ taobaoke/scripts/wx_chatroom/qrcode_manage.py
@@ -8,10 +8,12 @@ import sys
 sys.path.append('/home/new_taobaoke/taobaoke/')
 
 import django
+
 os.environ.update({"DJANGO_SETTINGS_MODULE": "fuli.settings"})
 django.setup()
 
 from broadcast.models.chatroom_models import ChatRoomQrcode
+
 logger = logging.getLogger('utils')
 
 get_date_url = 'http://s-prod-04.qunzhu666.com:10024/api/robot/get_chatroom_qrcode/'
@@ -28,15 +30,16 @@ if __name__ == '__main__':
         try:
             res = requests.post(get_date_url, json=postdate)
             return_data = res.json()
-
             print return_data
+
+            chatroom_id = '7218909824@chatroom'
             name = return_data['chatroom_name']
             url = return_data['qrcode_url']
             create_time = datetime.now()
 
-            chatroom_dict = {'chatroom_id': '7218909824@chatroom', 'name': name, 'qrcode_url': url,
+            chatroom_dict = {'chatroom_id': chatroom_id, 'name': name, 'qrcode_url': url,
                              'create_time': create_time}
-            ChatRoomQrcode.objects.update_or_create(name=name, defaults=chatroom_dict)
+            ChatRoomQrcode.objects.update_or_create(chatroom_id=chatroom_id, defaults=chatroom_dict)
 
             logger.info('response_state:' + str(res.status_code))
 
