903b28980e0e8b7556fb135f4c7e0b96b5f428e3
Adam <934399003@qq.com>
2018-01-23 15:56:44 +0800

    "[Update]转账用的新文件."

---
+++ taobaoke/apps/account/utils/transfer_utils.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+"""
+支付宝python sdk,第三方来源, 主页：https://github.com/fzlee/alipay/blob/master/README.zh-hans.md
+"""
+import time
+from datetime import datetime
+from alipay import AliPay
+from fuli.top_settings import alipay_appid
+
+def pay(account, name, amount):
+
+    alipay = AliPay(
+        appid=alipay_appid,
+        app_notify_url='',
+        app_private_key_path='apps/account/utils/ali_private_key.pem',
+        alipay_public_key_path='apps/account/utils/ali_public_key.pem',
+        sign_type="RSA2"
+    )
+
+    result = alipay.api_alipay_fund_trans_toaccount_transfer(
+        out_biz_no= str(int(time.mktime(datetime.now().timetuple()))),
+        payee_type="ALIPAY_LOGONID",
+        payee_account=account,
+        payee_real_name=name,
+        amount=amount,
+        payer_show_name='一起赚佣金',
+    )
+
+    return result
