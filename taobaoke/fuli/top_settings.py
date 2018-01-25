8bef222f2e6b1a466b03c2d6525b3768443f50a2
752546103 <752546103@qq.com>
2018-01-25 11:00:22 +0800

    [update]迁移机器人至皮卡丘，替换host_url

--- taobaoke/fuli/top_settings.py
+++ taobaoke/fuli/top_settings.py
@@ -5,7 +5,9 @@ app_secret = 'e9b343c489077ee3b54d21fee2115339'
 APPID = "wx4d49b4003114ca8a"
 APPSECRET = "96c99795528c07059d7f17cf73441b37"
 
-host = "http://s-prod-04.qunzhu666.com:10024"
+# host = "http://s-prod-04.qunzhu666.com:10024"
+host = "http://sv.pkq.qunzhu666.com"
+
 
 platform_list_url = host + "/api/robot/platform_user_list?platform_id="
 send_msg_url = host + "/api/robot/send_msg/"
