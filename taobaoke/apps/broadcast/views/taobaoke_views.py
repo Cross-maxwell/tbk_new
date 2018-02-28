b44024d8cb0188baa7f1740d2e8827a2e7205b70
Adam <934399003@qq.com>
2018-02-28 15:02:50 +0800

    "[Update]第三方消息添加关键词屏蔽"

--- taobaoke/apps/broadcast/views/taobaoke_views.py
+++ taobaoke/apps/broadcast/views/taobaoke_views.py
@@ -841,6 +841,10 @@ class AcceptThirdMsgView(View):
             return HttpResponse('Third Msg Switch Off')
         try:
             msg = MsgManager(request.body)
+            forbidden_keywords = ['私聊群主', '免费拿', '@']
+            for i in forbidden_keywords:
+                if i in msg:
+                    return HttpResponse('Forbidden by Keywords: {}'.format(i))
             item_id = msg.parse() # 完成消息解析及存库
             if item_id is not None:
                 requests.post('http://s-prod-07.qunzhu666.com/tk/send_artifical_msg', data=json.dumps({"item_id": item_id, "data":""}))
